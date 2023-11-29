94c2c2be4fd6f2004f7d735fc338682ab880ef46
Beatriz <martinezgarciabeatriz10@gmail.com>
2023-11-29 19:44:22 +0100

    archivos

--- archivos/users.sql
+++
@@ -1,7 +0,0 @@
-----------------
-USE USERS;
-INSERT INTO users ( email, telefono, nombre)
-VALUES ("martinezgarciabeatriz10@gmail.com", "682378597", "Beatriz Martinez Gracia", FALSE);
-
-
-SELECT * FROM users;
